a653933b0615d236099f9352d3638d53c5ba1962
nivabecassis <abecassis.niv@gmail.com>
2023-01-22 08:27:37 -0500

    Fix issues

--- frontend/src/components/Images/ImageViewer.tsx
+++ frontend/src/components/Images/ImageViewer.tsx
@@ -9,61 +9,61 @@ import { DefaultEventsMap } from "socket.io/dist/typed-events";
 import { Socket } from "socket.io-client";
 
 type IProps = {
-    socket: Socket<DefaultEventsMap, DefaultEventsMap> | undefined
-}
+  socket: Socket<DefaultEventsMap, DefaultEventsMap> | undefined;
+};
 
 function ImageViewer(props: IProps) {
-    const { socket } = props;
-    const [images, setImages] = useState<string[]>([]);
+  const { socket } = props;
+  const [images, setImages] = useState<string[]>([]);
 
-    useEffect(() => {
-        if (socket) {
-            socket.on("WorkFromHomeImages", (data: string[]) => {
-                setImages(data);
-            });
-        }
-    }, [socket]);
+  useEffect(() => {
+    if (socket) {
+      socket.on("WorkFromHomeImages", (data: string[]) => {
+        setImages(data);
+      });
+    }
+  }, [socket]);
 
-    return (
-        <Container>
-            <Header>#WorkStation {images.length}</Header>
-            <Slide {...properties}>
-                {images.map((each, index) => (
-                <EachSlide key={index} className="each-slide">
-                    <ImageSlide className="lazy" src={each} alt="sample" />
-                </EachSlide>
-                ))}
-            </Slide>
-        </Container>
-    );
+  return (
+    <Container>
+      <Header>#WorkStation</Header>
+      <Slide {...properties}>
+        {images.map((each, index) => (
+          <EachSlide key={index} className="each-slide">
+            <ImageSlide className="lazy" src={each} alt="sample" />
+          </EachSlide>
+        ))}
+      </Slide>
+    </Container>
+  );
 }
 
 const Container = styled(Widget)`
-    height: 100%;
-    width: 100%;  
-    padding: 15px;
-    /* grid-row: 1 / 3; */
-`
+  height: 100%;
+  width: 100%;
+  padding: 15px;
+  /* grid-row: 1 / 3; */
+`;
 
 const ImageSlide = styled.img`
-    margin-top: 10px;
-    max-width: 100%;
-    max-height: 360px;
-`
+  margin-top: 10px;
+  max-width: 100%;
+  max-height: 360px;
+`;
 
 const EachSlide = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const properties = {
-    duration: 5000,
-    autoplay: true,
-    transitionDuration: 500,
-    arrows: false,
-    infinite: true,
-    easing: "ease"
-  };
+  duration: 5000,
+  autoplay: true,
+  transitionDuration: 500,
+  arrows: false,
+  infinite: true,
+  easing: "ease",
+};
 
 export default ImageViewer;
--- frontend/src/styling/Theme.ts
+++ frontend/src/styling/Theme.ts
@@ -1,36 +1,36 @@
-import {DefaultTheme} from "styled-components";
+import { DefaultTheme } from "styled-components";
 
 export const Theme: DefaultTheme = {
-    main: {
-        colour: {
-            background: "#31353f"
-        }
+  main: {
+    colour: {
+      background: "#31353f",
     },
-    widget: {
-        colour: {
-            background: "#1b2028",
-            mainText: "#fff",
-            subText: "#b9b9b9",
-            subSubText: "#838689"     
-        },
+  },
+  widget: {
+    colour: {
+      background: "#1b2028d9",
+      mainText: "#fff",
+      subText: "#b9b9b9",
+      subSubText: "#838689",
     },
-    calendar: {
-        events: {
-            colour: {
-                background: "#31353f",
-                financial: "#9ABF97",
-                deadline: "#CF6363",
-                social: "#73ADBF" 
-            }
-        }
+  },
+  calendar: {
+    events: {
+      colour: {
+        background: "#31353f",
+        financial: "#9ABF97",
+        deadline: "#CF6363",
+        social: "#73ADBF",
+      },
     },
-    jira: {
-        project: {
-            colour: {
-                good: "#9ABF97",
-                warning: "#D99157",
-                alert: "#CF6363"
-            }
-        }
-    }
-}
+  },
+  jira: {
+    project: {
+      colour: {
+        good: "#9ABF97",
+        warning: "#D99157",
+        alert: "#CF6363",
+      },
+    },
+  },
+};
